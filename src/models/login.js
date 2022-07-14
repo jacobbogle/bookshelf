72c67d798318284f3d1d083964ce466ae89fe228
BlackErik <blac.erik@gmail.com>
2022-07-14 10:10:38 -0600

    GET /session working on front end, username being displayed

--- src/models/login.js
+++ src/models/login.js
@@ -1,46 +1,42 @@
 import { ref } from "vue";
 
-
-
-
 const Login = () => {
-    const state = ref({
-        currentUser: ""
-      });
+  const state = ref({
+    currentUser: "",
+  });
 
+  const postSession = async function (email, password) {
+    let loginCredentials = {
+      username: email,
+      password: password,
+    };
 
-    const postSession = async function (email, password) {
-        let loginCredentials = {
-            username: email,
-            password: password
-        };
-    
-        let response = await fetch("http://localhost:3000/session", {
-            method: "POST",
-            body: JSON.stringify(loginCredentials),
-            headers: {
-            "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
-    
-        if (response.status == 201) {
-            state.value.currentUser = loginCredentials.username;
-            console.log("SUCCESSFUL LOGIN ATTEMPT");
-        } else if (response.status == 401) {
-            console.log("UNSUCCESSFUL LOGIN ATTEMPT");
-        } else {
-            console.log(
-            "Some other error in POST /session",
-            response.status,
-            response
-            );
-        }
+    let response = await fetch("http://localhost:3000/session", {
+      method: "POST",
+      body: JSON.stringify(loginCredentials),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    if (response.status == 201) {
+      state.value.currentUser = loginCredentials.username;
+      console.log("SUCCESSFUL LOGIN ATTEMPT");
+    } else if (response.status == 401) {
+      console.log("UNSUCCESSFUL LOGIN ATTEMPT");
+    } else {
+      console.log(
+        "Some other error in POST /session",
+        response.status,
+        response
+      );
     }
+  };
 
-    return {
-        postSession
-    }
-}
+  return {
+    postSession,
+  };
+};
 
 export default Login;
