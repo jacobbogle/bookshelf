c1618fae40c49fc07b2a9edd7c24ed4d9bb90d95
jacob <jacobbogle95@gmail.com>
2022-07-14 00:07:37 -0600

    style fix

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,13 @@ export default {
 
 <style>
 
+
 #app {
     margin: 0;
       background-image: linear-gradient(to bottom right, 
     rgb(14, 14, 27), rgb(39, 37, 58));
     font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    height: 100vh;
 }
 
 .wrapper {
