e015343afa3c2e07215d2c36ecd14cd22dc82dc4
jacob <jacobbogle95@gmail.com>
2022-07-17 23:43:30 -0600

    still in the works

---
+++ src/components/Search-section.vue
@@ -0,0 +1,228 @@
+<template>
+  <div id="bookCollection">
+    <div
+        class="book-recommend"
+        v-for="(book, index) in state.books.items"
+        :key="index" 
+      >
+        <div ref="bookBox" class="book-box" @click="bookClickHandler(index)">
+          <img ref="image" :value="book.selfLink" class="book-image" :src="imageTest(book)" />
+        </div>
+        <div style="visibility: hidden" ref="bookContent" class="book-content">
+          <div class="text-content">
+            <h2 ref="title">{{ book.volumeInfo.title }}</h2>
+            <!-- Book Title -->
+            <h3 :value="book.volumeInfo.averageRating" ref="rating">
+              Rating: {{ numGive(book.volumeInfo.averageRating) }}
+            </h3>
+            <!-- Book Rating -->
+            <h4 ref="authors">By: {{ textGive(book.volumeInfo.authors) }}</h4>
+            <!-- Author's Name -->
+            <a
+              ref="link"
+              class="Amazon"
+              :href="`${book.volumeInfo.infoLink}`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Google Link</a
+            ><!-- Product Link -->
+            <button @click="serveBook(index), postBook(bookObject)">
+              Save Book
+            </button>
+            <!-- button to add to database -->
+            <p ref="description" v-html="snippetGive(book)"></p>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Search from "../models/search";
+export default {
+setup() {
+    const { state, postBook, searchByTitle,  } = Search();
+    return { state, postBook, searchByTitle,  };
+  },
+  data() {
+    return {
+      isBookOpen: false,
+      IndexOfOpenedBook: null,
+      books: [],
+      bookObject: {
+        image: "",
+        title: "",
+        rating: null,
+        authors: "",
+        link: "",
+        description: "",
+        isbn: "",
+      },
+    };
+  },
+  methods: {
+    serveBook(index) {
+      this.bookObject.image = String(this.$refs.image[index].src);
+      this.bookObject.title = this.$refs.title[index].innerHTML;
+      this.bookObject.rating = this.numGive(
+        this.state.books.items[index].volumeInfo.averageRating
+      );
+      this.bookObject.authors = this.textGive(
+        this.state.books.items[index].volumeInfo.authors
+      );
+      this.bookObject.link = this.$refs.link[index].href;
+      this.bookObject.description = this.snippetGive(
+        this.state.books.items[index]
+      );
+      this.bookObject.isbn = this.isbnGive(
+        this.state.books.items[index].volumeInfo.industryIdentifiers
+      );
+    },
+    isbnGive(text) {
+      try {
+        for (const i in text) {
+          if (text[i].type == "ISBN_13") {
+            return text[i].identifier;
+          }
+        }
+      } catch (error) {
+        return this.bookObject.link;
+      }
+    },
+    textGive(text) {
+      try {
+        let goodText = text.join(" ");
+        return goodText;
+      } catch (error) {
+        return "N/A";
+      }
+    },
+    snippetGive(book) {
+      try {
+        let snippet = book.volumeInfo.description;
+        return snippet;
+      } catch (error) {
+        return book.searchInfo.textSnippet;
+      }
+    },
+    numGive(num) {
+      try {
+        if (num > 0) {
+          return num + " // 5";
+        } else {
+          return "N/A";
+        }
+      } catch (error) {
+        return "N//A";
+      }
+    },
+    imageTest(book) {
+      try {
+        let thumbnail = book.volumeInfo.imageLinks.thumbnail;
+        return thumbnail;
+      } catch (error) {
+        return "https://cdn.pixabay.com/photo/2018/01/04/15/51/404-error-3060993_1280.png";
+      }
+    },
+    searchReset() {
+      this.isBookOpen = false;
+      this.IndexOfOpenedBook = null;
+    },
+    bookClickHandler(index) {
+      this.$emit("openBookContent");
+      if (this.IndexOfOpenedBook != index && this.isBookOpen === false) {
+        this.openBookContent(index);
+        this.IndexOfOpenedBook = index;
+        this.isBookOpen = true;
+        this.IndexOfOpenedBook = index;
+      } else if (this.IndexOfOpenedBook === index && this.isBookOpen === true) {
+        this.closeBookContent(index);
+        this.isBookOpen = false;
+      } else if (
+        this.IndexOfOpenedBook === index &&
+        this.isBookOpen === false
+      ) {
+        this.openBookContent(index);
+        this.isBookOpen = true;
+      } else {
+        try {
+          this.closeBookContent(this.IndexOfOpenedBook);
+          this.openBookContent(index);
+          this.IndexOfOpenedBook = index;
+        } catch (error) {
+          null;
+        }
+      }
+    },
+    openBookContent(index) {
+      this.$emit("openBookContent");
+      if (window.innerWidth <= 684) {
+        this.$refs.bookContent[index].style.height = "390px";
+      } else {
+        this.$refs.bookContent[index].style.width = "256px";
+      }
+      this.$refs.bookContent[index].style.visibility = "visible";
+    },
+    closeBookContent(index) {
+      this.$emit("closeBookContent");
+      if (window.innerWidth <= 684) {
+        this.$refs.bookContent[index].style.width = "256px";
+        this.$refs.bookContent[index].style.height = "0px";
+      } else {
+        this.$refs.bookContent[index].style.width = "0px";
+      }
+      this.$refs.bookContent[index].style.visibility = "hidden";
+    },
+    closeAllBooks() {
+      if (this.isBookOpen === true) {
+        this.closeBookContent(this.IndexOfOpenedBook);
+        this.isBookOpen = false;
+      }
+    },
+    // async imageUpdate() {
+    //   try {
+    //     let linksList = []
+    //     for (let index = 0; index < this.state.lengthOfBooks; index++) {
+    //       const element = this.$refs.image[index].attributes[2].value;
+    //       linksList.push(element)
+    //     }
+    //     if (this.state.lengthOfBooks > 0) {
+    //       let newImageList = await this.newLinkGetter(linksList)
+    //       for (let index = 0; index < this.state.lengthOfBooks.length; index++) {
+    //         const element = newImageList[index];
+    //         console.log(element)
+    //         this.$refs.image[index].src = element
+    //         console.log(this.$refs.image[index])
+    //       }
+    //     }
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+  },
+//   updated() {
+//     // this.imageUpdate()
+//   },
+    async updated() {
+        await this.searchByTitle();
+    }
+};
+</script>
+
+<style scoped>
+@import "../book-data/style.css";
+
+#bookCollection {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+}
+
+#bookCollection > * {
+  margin: 0.5rem;
+}
+
+</style>
--- src/models/search.js
+++ src/models/search.js
@@ -5,10 +5,11 @@ const api = require("../../api.config");
 const googleApiKey = api["API_KEY"];
 const googleBooksURL = "https://www.googleapis.com/books/v1";
 
-const SearchBar = () => {
+const Search = () => {
   const state = ref({
     searchTitle: "",
     books: {},
+    lengthOfBooks: null
   });
 
   //makes the querystring and adds the needed api string for the GET of google books api
@@ -17,13 +18,13 @@ const SearchBar = () => {
   function urlString() {
     let title = encodeURIComponent(state.value.searchTitle);
     let key = googleApiKey;
-    return "?q=" + title + "&maxResults=11" + "&key=" + key;
+    return "?q=" + title + "&maxResults=10" + "&key=" + key;
   }
 
   //returns a json file of related books from searchTitle
   const searchByTitle = async () => {
     try {
-      let URLSTRING = urlString();
+      let URLSTRING =  await urlString();
       await fetch(googleBooksURL + "/volumes" + URLSTRING)
         .then((res) => {
           let response = res.json();
@@ -31,12 +32,50 @@ const SearchBar = () => {
         })
         .then((data) => {
           state.value.books = data;
+          state.value.lengthOfBooks = (data.items).length
           console.log(data);
         });
     } catch (err) {
       console.log(err);
     }
   };
+
+  const newObjectkGetter = async (listOfLinks) => {
+    console.log(listOfLinks)
+    let NewBooksObject = []
+    for (let index = 0; index < state.value.lengthOfBooks; index++) {
+      const link = listOfLinks[index];
+      await fetch(link).then((res) => {
+        let response =  res.json();
+        NewBooksObject.push(response)
+        console.log("Good fetch on book: " + (index+1));
+        return response;
+      })
+      //console.log(bookObject)
+    }
+  }
+
+
+  // const newImageGetter = async (listOfLinks) => {
+  //   try {
+  //     let bookImageDict = bookObject.volumeInfo.imageLinks
+  //     if (Object.hasOwnProperty.call(bookImageDict, "medium")) {
+  //       newImageLinkList.push(bookImageDict["medium"])
+  //       // console.log("saved medium: " + bookImageDict["medium"])
+  //     } else if (Object.hasOwnProperty.call(bookImageDict, "small")) {
+  //       newImageLinkList.push(bookImageDict["small"])
+  //       // console.log("save small: " + bookImageDict["small"])
+  //     } else if (Object.hasOwnProperty.call(bookImageDict, "thumbnail")) {
+  //       newImageLinkList.push(bookImageDict["thumbnail"])
+  //       // console.log("save thumbnail: " + bookImageDict["thumbnail"])
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     newImageLinkList.push("https://cdn.pixabay.com/photo/2018/01/04/15/51/404-error-3060993_1280.png")
+  //     console.log("save: Defualt")
+  //   }
+  // }
+
 
   const postBook = async function (book) {
     let newBook = {
@@ -65,7 +104,8 @@ const SearchBar = () => {
     state,
     searchByTitle,
     postBook,
+    newObjectkGetter,
   };
 };
 
-export default SearchBar;
+export default Search;
