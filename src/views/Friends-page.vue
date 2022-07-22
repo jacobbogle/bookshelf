0b68f5d952922d2f34aa09c6598bd19e7ffbcd4b
BlackErik <blac.erik@gmail.com>
2022-07-22 10:22:20 -0600

    h1's only show if there is something in the friends/received/sent categories

--- src/views/Friends-page.vue
+++ src/views/Friends-page.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="wrapper">
-    <h1>Friends</h1>
+    <h1 v-if="friendsExist">Friends</h1>
     <div v-for="(friend, index) in friends" :key="index">
       <h2 v-if="friend.value == 3">{{ friend.name }}</h2>
     </div>
 
-    <h1>Received Friend Requests</h1>
+    <h1 v-if="receivedsExist">Received Friend Requests</h1>
     <div v-for="(friend, index) in friends" :key="index">
       <h2 v-if="friend.value == 1">{{ friend.name }}</h2>
       <button v-if="friend.value == 1" @click="acceptFriendRequest(friend.id)">
@@ -16,7 +16,7 @@
       </button>
     </div>
 
-    <h1>Sent Friend Requests</h1>
+    <h1 v-if="sentsExist">Sent Friend Requests</h1>
     <div v-for="(friend, index) in friends" :key="index">
       <h2 v-if="friend.value == 2">{{ friend.name }}</h2>
     </div>
@@ -59,6 +59,40 @@ export default {
       let data = await response.json();
       console.log("denyFriendRequest Resposne: ", data);
       this.getFriends();
+    },
+  },
+  computed: {
+    friendsExist: function () {
+      let aFriendExists = false;
+      for (const friend in this.friends) {
+        if (this.friends[friend].value == 3) {
+          aFriendExists = true;
+          return aFriendExists;
+        }
+      }
+      return aFriendExists;
+    },
+
+    receivedsExist: function () {
+      let receivedFriend = false;
+      for (const friend in this.friends) {
+        if (this.friends[friend].value == 1) {
+          receivedFriend = true;
+          return receivedFriend;
+        }
+      }
+      return receivedFriend;
+    },
+
+    sentsExist: function () {
+      let sentFriend = false;
+      for (const friend in this.friends) {
+        if (this.friends[friend].value == 2) {
+          sentFriend = true;
+          return sentFriend;
+        }
+      }
+      return sentFriend;
     },
   },
 
