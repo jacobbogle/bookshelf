8f28c1d3118a733d79baf34c3c3ca2b3865b8c5b
jacobbogle <jacobbogle95@gmail.com>
2022-07-23 14:51:16 -0600

    inti

--- src/views/Login-page.vue
+++ src/views/Login-page.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="wrapper">
     <h1>Hello</h1>
-    <form class="centered">
-      <label for="email">Email</label>
-      <input
-        v-model="email"
+    <el-form class="centered">
+      <el-form-item label="Email">
+        <el-input :model="email"
         placeholder="type here..."
         type="text"
         id="email"
         name="email"
-        required
-      />
-      <label for="password">Password</label>
-      <input
-        v-model="password"
+        required></el-input>
+      </el-form-item>
+
+      <el-form-item label="Password">
+        <el-input :model="password"
         placeholder="type here..."
-        type="password"
-        id="password"
-        name="password"
-        required
-      />
-      <button @click="loginFunc()" type="submit">Sign In</button>
-    </form>
+        type="text"
+        id="email"
+        name="email"
+        required></el-input>
+      </el-form-item>
+
+      <el-button @click="loginFunc()" type="primay">Sign In</el-button>
+
+    </el-form>
     <br>
     <p>Don't have an acount?</p>
     <router-link id="link" to="/register">
@@ -36,25 +37,25 @@ import Login from "../models/login";
 import User from "../models/user"
 
 export default {
-  setup() {
-    const { state, postSession } = Login();
-    const { getSession } = User();
-    return { state, postSession, getSession };
-  },
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    loginFunc() {
-      this.postSession(this.email, this.password)
+    setup() {
+        const { state, postSession } = Login();
+        const { getSession } = User();
+        return { state, postSession, getSession };
     },
-    async google() {
-      await window.open('http://localhost:3000/auth/google', 'newwindow','width=500,height=600')
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
     },
-  },
+    methods: {
+        loginFunc() {
+            this.postSession(this.email, this.password);
+        },
+        async google() {
+            await window.open("http://localhost:3000/auth/google", "newwindow", "width=500,height=600");
+        },
+    },
 }
 </script>
 
