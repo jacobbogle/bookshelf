d25774118e036a4698013379365f4cff06e52596
BlackErik <blac.erik@gmail.com>
2022-07-27 09:34:58 -0600

    removed some console.logs

--- src/views/Home-page.vue
+++ src/views/Home-page.vue
@@ -93,8 +93,6 @@ export default {
       let data = await response.json();
       let newData = [];
       for (let i in data) {
-        console.log(data[i].books);
-        console.log(data[i].books.length);
         if (data[i].books.length === 0) {
           console.log("data is empty");
         } else {
@@ -102,8 +100,6 @@ export default {
         }
       }
       this.books = this.shuffle(newData);
-      console.log(data);
-      console.log(newData);
     },
     getUsername() {},
     reset() {
